abe2946a4a1659292702d5d241a095d3056637ac
Sukwon Choi <swchoi06@snatcher.ai>
2019-04-16 15:44:45 +0900

    Added dataset

---
+++ 3/point_cloud/cloud.js
@@ -0,0 +1,28 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "points": 401791,
+    "boundingBox": {
+        "lx": 319962.153863,
+        "ly": 5181249.26753,
+        "lz": 483.296826019,
+        "ux": 320181.346822,
+        "uy": 5181468.460489,
+        "uz": 702.4897850190073
+    },
+    "tightBoundingBox": {
+        "lx": 319962.15388,
+        "ly": 5181249.26544,
+        "lz": 483.3,
+        "ux": 320181.34388,
+        "uy": 5181431.07544,
+        "uz": 499.64
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 1.518613338470459,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
