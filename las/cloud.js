df4e60b054e6d70189c7cff30fb8a5058df98d9e
Sukwon Choi <swchoi06@snatcher.ai>
2019-04-18 09:17:00 +0900

    Added dataset

---
+++ las/cloud.js
@@ -0,0 +1,29 @@
+{
+    "version": "1.7",
+    "octreeDir": "data",
+    "projection": "",
+    "points": 377668,
+    "boundingBox": {
+        "lx": 365366.962389,
+        "ly": 3870478.55547,
+        "lz": 37.8896956316,
+        "ux": 365482.095526,
+        "uy": 3870593.688607,
+        "uz": 153.0228326316262
+    },
+    "tightBoundingBox": {
+        "lx": 365366.967334,
+        "ly": 3870478.55811,
+        "lz": 37.89,
+        "ux": 365482.097334,
+        "uy": 3870520.2381100005,
+        "uz": 130.77
+    },
+    "pointAttributes": [
+        "POSITION_CARTESIAN",
+        "COLOR_PACKED"
+    ],
+    "spacing": 0.9970822334289551,
+    "scale": 0.001,
+    "hierarchyStepSize": 5
+}
